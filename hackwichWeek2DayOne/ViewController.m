9ca94ad592e758120f1001c662e98368ac2bd137
Mike Land <mikepland@gmail.com>
2014-10-11 14:17:21 -0500

    sets up basic table

--- hackwichWeek2DayOne/ViewController.m
+++ hackwichWeek2DayOne/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
 
 @end
 
@@ -19,9 +19,15 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 3;
 }
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MyCellID" forIndexPath:indexPath];
+    cell.textLabel.text = [NSString stringWithFormat:@"Row %li", (long)indexPath.row];
+    return cell;
+}
+
 
 @end
